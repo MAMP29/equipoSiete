d0e8c33d4352c5463e5804550ac111cb86b2b5d3
MAMP29 <106500061+MAMP29@users.noreply.github.com>
2025-11-03 12:16:05 -0500

    Buton de guardar añadido y clase fragment addProduct ajustada

--- app/src/main/java/com/appmovil/inventorywidget/view/fragment/addProduct.kt
+++ app/src/main/java/com/appmovil/inventorywidget/view/fragment/addProduct.kt
@@ -6,55 +6,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.appmovil.inventorywidget.R
+import com.appmovil.inventorywidget.databinding.FragmentAddProductBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
- * A simple [Fragment] subclass.
- * Use the [addProduct.newInstance] factory method to
- * create an instance of this fragment.
+ * Fragment para  añadir un producto
  */
 class addProduct : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    // Manejo explicito del biding, para mamenjo contra null
+    // https://developer.android.com/topic/libraries/view-binding?hl=es-419#fragments
+    private var _binding: FragmentAddProductBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_product, container, false)
+        // Inflate convierte el XML a vistas reales, biding hace el inflate de forma automatica
+        _binding = FragmentAddProductBinding.inflate(inflater, container, false)
+
+        // Vista raiz
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment addProduct.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            addProduct().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    // Limpiando la referencia del binding
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
