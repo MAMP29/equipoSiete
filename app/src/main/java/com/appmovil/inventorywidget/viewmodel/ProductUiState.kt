877814efd9c43f60c6c1225d6fea359e3c2ea61b
MAMP29 <106500061+MAMP29@users.noreply.github.com>
2025-11-09 20:57:34 -0500

    Progress circle implementado al cargar la lista en el fragment de inventario

---
+++ app/src/main/java/com/appmovil/inventorywidget/viewmodel/ProductUiState.kt
@@ -0,0 +1,8 @@
+package com.appmovil.inventorywidget.viewmodel
+
+import com.appmovil.inventorywidget.model.Product
+
+sealed class ProductUiState {
+    object Loading : ProductUiState()
+    data class Success(val products: List<Product>) : ProductUiState()
+}
